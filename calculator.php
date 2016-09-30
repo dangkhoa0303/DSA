2b7ce3aa156e0f85f03da88d3ef37b8388c5be60
Khoa Phung <Khoa2.Phung@live.uwe.ac.uk>
2016-09-30 13:44:13 +0100

    Added calculator.php

---
+++ calculator.php
@@ -0,0 +1,97 @@
+<?php 
+
+/* ====================================================== 
+   PHP Calculator example using "sticky" form (Version 1) 
+   ====================================================== 
+
+   Author : P Chatterjee (adopted from an original example written by C J Wallace)
+
+   Purpose : To multiply 2 numbers passed from a HTML form and display the result.
+
+   input: 
+      x, y : numbers 
+      calc : Calculate button pressed 
+
+
+   Date: 15 Oct 2007 
+
+*/ 
+
+// grab the form values from $_HTTP_POST_VARS hash 
+extract($_GET); 
+$prod = 0;
+
+// first compute the output, but only if data has been input 
+   if(isset($calc)) { // $calc exists as a variable 
+      calculate($x, $y, $op, $prod);
+   } 
+   else { // set defaults 
+      $x=0; 
+      $y=0; 
+      $prod=0;
+   }
+
+function calculate($x, $y, $op, &$prod) {
+    switch($op) {
+        case '+':
+            $prod = $x + $y;
+            break;
+        case '-':
+            $prod = $x - $y; 
+            break;
+        case '*':
+            $prod = $x * $y; 
+            break;
+        case '/':
+            if ($y==0) {
+                $prod = "&infin;";
+            } else {
+                $prod = $x / $y; 
+            }
+            break;
+        default:
+            $prod = 0;
+            break;
+    }
+}
+?> 
+
+<html> 
+   <head> 
+      <title>PHP Calculator Example</title> 
+   </head> 
+
+   <body> 
+
+      <h3>PHP Calculator (Version 1)</h3>  
+
+      <form method="get" action="<?php print $_SERVER['PHP_SELF']; ?>"> 
+
+         x = <input type="text" name="x" size="5" value="<?php print $x; ?>"/> 
+         
+          op =
+        <select name="op" size="1">
+             <option value="+" <?php if (isset($op) && $op=="+") echo "selected";?>>+</option>
+             <option value="-" <?php if (isset($op) && $op=="-") echo "selected";?>>-</option>
+             <option value="*" <?php if (isset($op) && $op=="*") echo "selected";?>>*</option>
+             <option value="/" <?php if (isset($op) && $op=="/") echo "selected";?>>/</option>
+         </select>
+         
+         y =  <input type="text" name="y" size="5" value="<?php  print $y; ?>"/> 
+
+
+         <input type="submit" name="calc" value="Calculate"/> 
+      </form> 
+
+      <!-- print the result --> 
+      <?php if(isset($calc)) { 
+         if ($op != "") {
+             print "<p>x $op y = $prod</p>";
+         } else {
+             print "Operator not chosen";
+         }
+
+      } ?> 
+
+   </body> 
+</html> 
