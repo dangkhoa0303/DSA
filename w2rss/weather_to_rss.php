7501569b5a51644d6b4fecf28bac8bd8182f3bf2
Khoa Phung <Khoa2.Phung@live.uwe.ac.uk>
2016-10-14 13:54:23 +0100

    Added w2rss folder - workshop 4

---
+++ w2rss/weather_to_rss.php
@@ -0,0 +1,27 @@
+<?php 
+require("wdllib.php"); 
+
+//main 
+  $site = $_REQUEST["site"]; 
+  $rawdatafile = $site . "http://www.meteomaspalomas.com/clientraw.txt"; 
+  $csv = file_get_contents($rawdatafile); 
+  $data = explode(' ',$csv); 
+
+  $data[SUMMARY] = str_replace('_', ' ',$data[SUMMARY]);
+  $data[WINDDIRECTION] = degree_to_compass_point($data[WINDDIRECTION]);
+    
+// generate the RSS  
+print <<<EOT
+<?xml version="1.0"?>  
+<rss version="2.0"> 
+  <channel> 
+      <title>{$data[STATION]}</title> 
+      <link>{$site}</link> 
+      <item> 
+         <title>Weather at {$data[TIMEHH]}:{$data[TIMEMM]}</title> 
+         <description> {$data[SUMMARY]} . Wind {$data[WINDSPEED]} knots from {$data[WINDDIRECTION]} . Temperature {$data[TEMPERATURE]} &#0176;C. </description> 
+      </item> 
+  </channel> 
+ </rss> 
+EOT;
+?>
